9d4022c01f8d3a087005afd74f8462eb0e6e743e
takutosugawara <tatakuku0724@gmail.com>
2025-05-14 14:52:54 +0900

    fix downloader

--- data/downloader.py
+++ data/downloader.py
@@ -1,14 +1,26 @@
+import json
+import os
+import time
+import tqdm
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-# Set up Chrome options
-options = Options()
-options.add_argument("--headless")  # Run headless Chrome
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+class Downloader:
+    def __init__(self, urls, checkpoint_file="checkpoint.json"):
+        self.urls = urls
+        self.checkpoint = checkpoint_file
+        self.downloaded = set()
+        self.load_checkpoint()
+
+        options = Options()
+        options.add_argument("--headless") # Run headless Chrome
+
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=options)
 
 
 # drive to a website 
