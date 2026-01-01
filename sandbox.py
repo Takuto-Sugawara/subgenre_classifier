107eb60392bc00afedcdce7182867f19b50e3302
takutosugawara <tatakuku0724@gmail.com>
2026-01-02 07:47:07 +0900

    fix: update environment variable and model names in sandbox.py; clean up .gitignore and add downloader_v3.py

--- sandbox.py
+++ sandbox.py
@@ -5,20 +5,15 @@ from transformers.image_utils import load_image
 import os
 
 
-token = os.getenv("token3")
+token = os.getenv("hf_token")
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = load_image(url)
 
-processor = AutoImageProcessor.from_pretrained("facebook/dinov3-vitsplus-pretrain-lvd1689m")
-
-quant_type = Int4WeightOnlyConfig(group_size=128)
-quantization_config = TorchAoConfig(quant_type=quant_type)
-
+pretrained_model_name = "facebook/dinov3-vits16-pretrain-lvd1689m"
+processor = AutoImageProcessor.from_pretrained(pretrained_model_name)
 model = AutoModel.from_pretrained(
-    "facebook/dinov3-vit7b16-pretrain-lvd1689m",
-    dtype=torch.bfloat16,
-    device_map="auto",
-    quantization_config=quantization_config
+    pretrained_model_name, 
+    device_map="auto", 
 )
 
 inputs = processor(images=image, return_tensors="pt").to(model.device)
